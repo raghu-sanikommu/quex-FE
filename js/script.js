e8c31b3f7c9a46fd3cdd81c58c3e86b3095f23f5
raghu-sanikommu <66820717+raghu-sanikommu@users.noreply.github.com>
2020-07-20 15:53:48 +0530

    FE up

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-let askButton = document.getElementById('ask-button')
+const submitAsked = document.getElementById('submit-ask');
+const submitSearch = document.getElementById('submit-search');
+const questionAsked = document.getElementById('question-asked');
+
+let questionsCorner = {};
+let questionCount = 1;
+
+submitAsked.addEventListener('click', () => {
+        questionsCorner['Q' + questionCount] = questionAsked.value;
+        localStorage.setItem('Q' + questionCount, questionAsked.value);
+        questionCount += 1;
+        questionAsked.value = '';
+        console.log(questionsCorner);
+    }
+);
